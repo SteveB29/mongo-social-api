5016201907e5fc940582e7805535a319a44caf56
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-21 23:24:27 -0500

    added Thought model

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // add getter to format Date
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    }
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
