bcd45313c31e87e401dff8525aab7d9a34a7ace3
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-21 23:16:39 -0500

    exported User and added to index

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const User = new Schema(
+const UserSchema = new Schema(
   {
     username: {
       type: String,
@@ -36,4 +36,8 @@ const User = new Schema(
 
 User.virtual('friendCount').get(function() {
   return this.friends.length;
-})
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
