4dedd404775bbaec73e89cae0c945e7a8df33305
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-23 23:10:17 -0500

    added thought routing

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+} = require('../../controllers/thought-controllers');
+
+// get route and add new thought route, /api/thoughts
+router.route('/')
+  .get(getAllThoughts)
+  .post(addThought);
+
+// routes that need thought id, /api/thoughts/:id
+router.route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// route for reactions through thoughts (should this be own file?)
+router.route(':thoughtId/reactions')
+  .put(addReaction)
+  .delete(removeReaction);
