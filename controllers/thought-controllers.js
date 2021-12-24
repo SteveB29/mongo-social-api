2a415a83da6cef44d8b5bb33552ba098439bfe2a
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-23 23:49:44 -0500

    added controller functions for each route function

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -2,7 +2,31 @@ const { User, Thought, reactionSchema } = require('../models');
 
 const thoughtController = {
   getAllThoughts({ params, body}, res) {
-    res.json({ message: 'Thought controller working' })
+    res.json({ message: 'getAllThoughts working' })
+  },
+
+  getThoughtById({ params, body}, res) {
+    res.json({ message: 'getThoughtById working' })
+  },
+
+  addThought({ params, body}, res) {
+    res.json({ message: 'addThought working' })
+  },
+
+  updateThought({ params, body}, res) {
+    res.json({ message: 'updateThought working' })
+  },
+
+  deleteThought({ params, body}, res) {
+    res.json({ message: 'deleteThought working' })
+  },
+
+  addReaction({ params, body}, res) {
+    res.json({ message: 'addReaction working' })
+  },
+
+  deleteReaction({ params, body}, res) {
+    res.json({ message: 'deleteReaction working' })
   }
 };
 
--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,7 +1,33 @@
 const { User, Thought, reactionSchema } = require('../models');
 
 const userController = {
+  getAllUsers({ params, body}, res) {
+    res.json({ message: 'getAllUsers working' })
+  },
 
+  getUserById({ params, body}, res) {
+    res.json({ message: 'getUserById working' })
+  },
+
+  addUser({ params, body}, res) {
+    res.json({ message: 'addUser working' })
+  },
+
+  updateUser({ params, body}, res) {
+    res.json({ message: 'updateUser working' })
+  },
+
+  deleteUser({ params, body}, res) {
+    res.json({ message: 'deleteUser working' })
+  },
+
+  addFriend({ params, body}, res) {
+    res.json({ message: 'addFriend working' })
+  },
+
+  removeFriend({ params, body}, res) {
+    res.json({ message: 'removeFriend working' })
+  }
 };
 
 module.exports = userController;
