3485f28914d7e1f06f73a018b10b1fbdbba10ae5
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-24 14:59:14 -0500

    added updateUser controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -34,7 +34,19 @@ const userController = {
   },
 
   updateUser({ params, body}, res) {
-    res.json({ message: 'updateUser working' })
+    if (!mongoose.Types.ObjectId.isValid(params.id)) {
+      res.status(415).json({ message: 'Invalid user id' });
+      return;
+    }
+    User.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   },
 
   deleteUser({ params }, res) {
