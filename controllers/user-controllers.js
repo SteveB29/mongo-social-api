4829e9a4b58fe18b4e5dfcb5a7e52973917ee5b1
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-24 09:13:06 -0500

    added get one user controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,15 +1,30 @@
 const { User, Thought, reactionSchema } = require('../models');
 
 const userController = {
-  getAllUsers({ params, body}, res) {
+  getAllUsers(req, res) {
     User.find({})
       .select('-__v')
       .then(dbUserData => res.json(dbUserData))
       .catch(err => res.status(400).json(err));
   },
 
-  getUserById({ params, body}, res) {
-    res.json({ message: 'getUserById working' })
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+    .select('-__v')
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch(err => {
+      if (err.name === "CastError") {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.status(400).json(err)
+    });
   },
 
   addUser({ body }, res) {
