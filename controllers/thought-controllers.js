ec27dde038505c63731e532ce71206062c7eb891
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-24 21:39:47 -0500

    added some comments

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -79,6 +79,7 @@ const thoughtController = {
           res.status(404).json({ message: 'No thought found with this id' });
           return;
         }
+        // thoughtId is not removed from User thoughts array, find way to remove
         res.json(dbThoughtData);
       })
       .catch(err => res.status(400).json(err));
